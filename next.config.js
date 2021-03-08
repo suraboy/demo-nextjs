ec47bdb2c5127e42a2f78c8eeef6cb2aa589f97b
Sirichai Janpan <13_oy@hotmail.co.th>
2021-03-08 22:35:07 +0700

    fixed config api production

--- next.config.js
+++ next.config.js
@@ -4,13 +4,13 @@ const webpack = require('webpack')
 module.exports = {
     optionalCatchAll: true,
     env: {
-        'BACKEND_API': process.env.BACKEND_API ? process.env.BACKEND_API : 'http://localhost:6319'
+        'BACKEND_API': 'https://nestjs-suraboy.herokuapp.com'
     },
     async rewrites() {
         return [
             {
                 source: '/v1/:path*',
-                destination: process.env.BACKEND_API ? process.env.BACKEND_API : "http://localhost:6319" + '/v1/:path*' // Proxy to Backend
+                destination: 'https://nestjs-suraboy.herokuapp.com/v1/:path*' // Proxy to Backend
             }
         ]
     },
